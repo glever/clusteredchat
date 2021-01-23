29f8747e36b3723016ec132e68bee626ba7f38ab
Glen Vermeylen <glen.vermeylen@gmail.com>
2021-01-23 15:47:35 +0100

    initial commit

---
+++ service/src/main/java/clusteredchat/ClusteredchatApplication.java
@@ -0,0 +1,13 @@
+package clusteredchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteredchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClusteredchatApplication.class, args);
+    }
+
+}
---
+++ service/src/test/java/clusteredchat/ClusteredchatApplicationTests.java
@@ -0,0 +1,13 @@
+package clusteredchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
