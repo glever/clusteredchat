c7b89a2a1465fc7423822bd9161884fecd1f8cc6
Glen Vermeylen <glen.vermeylen@gmail.com>
2021-01-24 11:22:26 +0100

    follow ng2-stompjs example to set up fe <-> rabbitmq example

--- app/clusteredchat/src/app/messages/messages.component.ts
+++ app/clusteredchat/src/app/messages/messages.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {RxStompService} from "@stomp/ng2-stompjs";
+import { RxStompService } from '@stomp/ng2-stompjs';
+import { Message } from '@stomp/stompjs';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  public receivedMessages: string[] = [];
+  private topicSubscription?: Subscription;
 
-  constructor(private rxStompService: RxStompService) { }
+  constructor(private rxStompService: RxStompService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.topicSubscription = this.rxStompService.watch('/topic/demo').subscribe((message: Message) => {
+      this.receivedMessages.push(message.body);
+    });
   }
+
   onSendMessage() {
-    const message = `Message generated at ${new Date}`;
-    this.rxStompService.publish({destination: '/topic/demo', body: message});
+    const message = `Message generated at ${new Date()}`;
+    this.rxStompService.publish({ destination: '/topic/demo', body: message });
   }
 }
